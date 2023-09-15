8db5c1f042fca159772887c85f4f53aadcbaedbb
Andrew Lai <the.andrew.lai@gmail.com>
2023-09-15 13:36:13 -0700

    d
    dd

---
+++ FVJ/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Drink from "./components/Drink";
+
+function App() {
+  return (
+    <div>
+      <Drink></Drink>
+      <Drink></Drink>
+      <Drink></Drink>
+    </div>
+  );
+}
+
+export default App;
---
+++ FVJ/src/components/Drink.tsx
@@ -0,0 +1,10 @@
+function Drink() {
+  return (
+    <div>
+      <h1>Drink name</h1>
+      <h2>Drink description</h2>
+    </div>
+  );
+}
+
+export default Drink;
