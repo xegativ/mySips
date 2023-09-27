707319719aeb482ed96da4660d1d9fcc9db08c70
Andrew Lai <the.andrew.lai@gmail.com>
2023-09-26 23:51:00 -0700

    Star implementation

--- mySips/src/App.tsx
+++ mySips/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Drink from "./components/Drink";
 import AddNew from "./components/AddNew";
 
+import { useState } from "react";
+
 function App() {
-  const elements = [];
+  const elements: JSX.Element[] = [];
   const numberOfDrinks = 3;
   for (let i = 0; i < numberOfDrinks; i++) {
-    elements.push(<Drink></Drink>);
+    elements.push(<Drink key={i} number={i}></Drink>);
   }
   elements.push(<AddNew></AddNew>);
 
--- mySips/src/components/Drink.tsx
+++ mySips/src/components/Drink.tsx
@@ -1,128 +1,45 @@
 import { useState } from "react";
 
-function Drink() {
-  // const checkStar = (
-  //   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  // ) => {
-  //   console.log("Checking star!");
-  //   return <p>O</p>;
-  // };
+function Drink(props) {
+  const [starArr, setStarArr] = useState([false, false, false, false, false]);
 
-  // const checkAllStars = () => {};
+  const onClickStar = (index: number) => {
+    const updatedArr = starArr.map((item, i) => {
+      if (index <= i) {
+        item = true;
+      } else {
+        item = false;
+      }
+      return item;
+    });
 
-  // const setStarArray = (
-  //   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  // ) => {
-  //   var stars = [];
-  //   var total = parseInt(event.currentTarget.id);
-  //   for (let i = 0; i < 5; i++) {
-  //     if (total > 0) {
-  //       stars.push(true);
-  //       total = total - 1;
-  //     } else {
-  //       stars.push(false);
-  //     }
-  //   }
-  //   console.log(stars);
-  // };
-  // const initStar = () => {
-  //   var stars = [true, false, false, true, false];
-  //   var falseRemain = false;
-
-  //   console.log("BEGIN CHECK");
-
-  //   for (let i = 0; i < stars.length; i++) {
-  //     console.log(stars[i]);
-  //     if (stars[i] == true && falseRemain == false) {
-  //       stars[i] = true;
-  //     } else if (stars[i] == false && falseRemain == false) {
-  //       falseRemain = true;
-  //     } else if (stars[i] == true && falseRemain == true) {
-  //       stars[i] = false;
-  //     }
-  //   }
-  //   console.log("END CHECK");
-  // };
-
-  const [starVar1, setStar1Var] = useState("X");
-  const [starVar2, setStar2Var] = useState("X");
-  const [starVar3, setStar3Var] = useState("X");
-  const [starVar4, setStar4Var] = useState("X");
-  const [starVar5, setStar5Var] = useState("X");
-
-  const changeStar1 = () => {
-    setStar1Var("X");
-    setStar2Var("O");
-    setStar3Var("O");
-    setStar4Var("O");
-    setStar5Var("O");
-  };
-
-  const changeStar2 = () => {
-    setStar1Var("X");
-    setStar2Var("X");
-    setStar3Var("O");
-    setStar4Var("O");
-    setStar5Var("O");
-  };
-
-  const changeStar3 = () => {
-    setStar1Var("X");
-    setStar2Var("X");
-    setStar3Var("X");
-    setStar4Var("O");
-    setStar5Var("O");
-  };
-
-  const changeStar4 = () => {
-    setStar1Var("X");
-    setStar2Var("X");
-    setStar3Var("X");
-    setStar4Var("X");
-    setStar5Var("O");
-  };
-
-  const changeStar5 = () => {
-    setStar1Var("X");
-    setStar2Var("X");
-    setStar3Var("X");
-    setStar4Var("X");
-    setStar5Var("X");
+    setStarArr(updatedArr);
   };
 
   return (
     <div className="drinkBox">
       <div className="dB-navbar">
-        <p>Drink Navbar</p>
-        <div>
-          <p>_</p>
-        </div>
-        <div>
-          <p>□</p>
-        </div>
-        <div>
-          <p>X</p>
-        </div>
+        <p>00{props.number}</p>
       </div>
 
       <div className="dB-content">
         <h1>Drink name</h1>
         <h2>Drink description</h2>
-        <button id="1" onClick={() => changeStar1()}>
-          {starVar1}
-        </button>
-        <button id="2" onClick={() => changeStar2()}>
-          {starVar2}
-        </button>
-        <button id="3" onClick={() => changeStar3()}>
-          {starVar3}
-        </button>
-        <button id="4" onClick={() => changeStar4()}>
-          {starVar4}
-        </button>
-        <button id="5" onClick={() => changeStar5()}>
-          {starVar5}
-        </button>
+        <div className="star-div">
+          {starArr
+            .map((item, index) => {
+              return (
+                <div
+                  className="star"
+                  key={index}
+                  onClick={() => onClickStar(index)}
+                >
+                  {item ? "[X]" : "[ ]"}
+                </div>
+              );
+            })
+            .reverse()}
+        </div>
       </div>
     </div>
   );
