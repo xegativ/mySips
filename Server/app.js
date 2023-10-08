2b81221866bbb5ac9cd3a269933017742c51ea60
Andrew Lai <the.andrew.lai@gmail.com>
2023-10-07 22:25:25 -0700

    Added create new user and get user

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const { v4: uuidv4 } = require('uuid');
 
 // create connection
 // - takes in configuration object; check documentation for param
 const db = mysql.createConnection({
     //empty for now
+    // will include either .env variables or AWS
 
 });
 
@@ -24,17 +26,80 @@ app.listen('3000', () => {
     console.log("Server starting on port 3000");
 });
 
-app.get('/adduser1', (req, res) => {
-    let post = {user_id: 1, username: "User 1", password: "hashed_password", userdata: null};
-    let sql = 'INSERT INTO users SET ?';
-    let query = db.query(sql, post, (err, result) => {
+
+// add user -> post 
+// /adduser?username=myUsername&password=myHashedPassword
+app.post('/adduser', (req, res) => {
+
+    // assume password already hashed for now
+    const { username, password } = req.query;
+
+    // user_id
+    const user_id = uuidv4();
+
+    // if no username or password
+    if (!username || !password) {
+        res.status(400).json({ error: 'Username and password are required.' });
+        return;
+    }
+
+    const sql = 'INSERT INTO users SET ?';
+    const post = { user_id: user_id, username: username, password: password, userdata: null };
+
+    // post to db
+    db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(result);
+            res.send('User created...');
+        }
+    });
+
+})
+
+
+// add user -> post 
+// /adduser?username=myUsername&password=myHashedPassword
+app.post('/getuser', (req, res) => {
+
+    // assume password already hashed for now
+    const { username, password } = req.query;
+
+    // user_id
+    const user_id = uuidv4();
+
+    // if no username or password
+    if (!username || !password) {
+        res.status(400).json({ error: 'Username and password are required.' });
+        return;
+    }
+
+    const sql = 'INSERT INTO users SET ?';
+    const post = { user_id: user_id, username: username, password: password, userdata: null };
+
+    // post to db
+    db.query(sql, post, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(result);
+            res.send('User created...');
+        }
+    });
+
+})
+
+// retrieve user by id 
+app.get('/getuser/:user_id', (req, res) => {
+    let sql = 'SELECT * FROM  users WHERE user_id = ${req.params.user_id}';
+    let query = db.query(sql, (err, result) => {
         if(err){
             throw err;
         }
         else {
             console.log(result);
-            res.send('User created...')
+            res.send('User fetched...')
         }
     })
 })
-
