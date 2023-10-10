19ee0b9d2776871c8e0d500260d7ffbcdbbf9a49
Andrew Lai <the.andrew.lai@gmail.com>
2023-10-09 20:07:12 -0700

    Fixed routing

--- mySips/src/main.tsx
+++ mySips/src/main.tsx
@@ -9,30 +9,30 @@ import Home from "./routes/Home.tsx";
 import Register from "./routes/Register.tsx";
 import Login from "./routes/Login.tsx";
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
-  {
-    path: "/app",
-    element: <App></App>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
-  {
-    path: "/register",
-    element: <Register></Register>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
-  {
-    path: "/login",
-    element: <Login></Login>,
-    errorElement: <ErrorPage></ErrorPage>,
-  },
+    {
+        path: "/",
+        element: <Home></Home>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/app",
+        element: <App></App>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/register",
+        element: <Register></Register>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/login",
+        element: <Login></Login>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
--- mySips/src/routes/Register.tsx
+++ mySips/src/routes/Register.tsx
@@ -1,5 +1,5 @@
 function Register() {
-  return <div>Login</div>;
+    return <div>Register</div>;
 }
 
 export default Register;
